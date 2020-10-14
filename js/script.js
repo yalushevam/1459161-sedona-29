6cb9682fffa90735aec9fa13cb55b9a24905d650
yalusheva <64979832+yalushevam@users.noreply.github.com>
2020-10-14 21:28:08 +0300

    js2

--- js/script.js
+++ js/script.js
@@ -3,10 +3,9 @@ const popup = document.querySelector('.modal');
 
 buttonModal.addEventListener('click', function (evt) {
 evt.preventDefault();
-popup.classList.add('modal-unactive');
-buttonModal.addEventListener('click', function () {
-      if (popup.classList.contains('modal-unactive')) {
-popup.classList.remove('modal-unactive');
-      }
-})
-});
+popup.classList.toggle('modal-unactive');
+popup.classList.toggle('modal');
+}
+);
+
+
