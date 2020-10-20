4f6a3be6fa33936ecf898732917427b6abea0abc
yalusheva <64979832+yalushevam@users.noreply.github.com>
2020-10-20 15:45:53 +0300

    ПП

--- js/script.js
+++ js/script.js
@@ -2,10 +2,7 @@ const buttonModal = document.querySelector('.button-search');
 const popup = document.querySelector('.modal-open');
 
 buttonModal.addEventListener('click', function (evt) {
-evt.preventDefault();
-popup.classList.toggle('modal-close');
-popup.classList.toggle('modal-open');
-}
-);
-
-
+    evt.preventDefault();
+    popup.classList.toggle('modal-close');
+    popup.classList.toggle('modal-open');
+});
