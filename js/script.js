c4dee0316931e18c4a8724e89c603c8cf981a6b8
yalusheva <64979832+yalushevam@users.noreply.github.com>
2020-10-24 19:06:24 +0300

    анимация
    пока не работает

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 const buttonModal = document.querySelector('.button-search');
 const popup = document.querySelector('.modal-open');
+const DateIn = popup.querySelector('[name=date-arrival]');
+const DateOut = popup.querySelector('[name=date-leaving]');
+const NumberAdults = popup.querySelector('[name=adult]');
+const NumberChildren = popup.querySelector('[name=child]');
 
 buttonModal.addEventListener('click', function (evt) {
     evt.preventDefault();
     popup.classList.toggle('modal-close');
     popup.classList.toggle('modal-open');
+    popup.classList.remove('modal-error');
+});
+
+popup.addEventListener('submit', function (evt) {
+  if (!DateIn.value || !DateOut.value || !NumberAdults.value || !NumberChildren.value) {
+  evt.preventDefault();
+  popup.classList.add('modal-error');
+  }
 });
